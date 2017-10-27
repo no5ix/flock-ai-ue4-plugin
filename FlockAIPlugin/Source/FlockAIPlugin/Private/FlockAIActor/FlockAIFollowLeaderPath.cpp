d058750997d92dd567c77e8b41110b847ca3a1f0
jypjiang <jyp_jiang@163.com>
2017-10-28 00:54:12 +0800

    删除了错误的提交

--- FlockAIPlugin/Source/FlockAIPlugin/Private/FlockAIActor/FlockAIFollowLeaderPath.cpp
+++
@@ -1,6 +0,0 @@
-
-#include "FlockAIPlugin.h"
-#include "FlockAIFollowLeaderPath.h"
-
-
-
--- FlockAIPlugin/Source/FlockAIPlugin/Private/FlockAIManager.cpp
+++
@@ -1,8 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "FlockAIPlugin.h"
-#include "FlockAIManager.h"
-
-
-
-
